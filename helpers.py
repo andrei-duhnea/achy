138f77e6e821eca659a5c97b71ff0c3b7d087e46
Andrei Duhnea <andrei@duhnea.net>
2015-09-07 05:57:20 +0300

    Added helpers

--- helpers.py
+++ helpers.py
@@ -1,7 +1,9 @@
+import uuid
+from datetime import datetime
+import time
 
 
 class Dotable(dict):
-
     __getattr__ = dict.__getitem__
 
     def __init__(self, d):
@@ -16,3 +18,24 @@ class Dotable(dict):
             return [cls.parse(i) for i in v]
         else:
             return v
+
+
+def unique_string(prefix='', length=0):
+    if length == 0:
+        return prefix + str(uuid.uuid4()).replace('-', '')
+    else:
+        return prefix + str(uuid.uuid4()).replace('-', '')[0:(length - len(prefix))]
+
+
+def unique_tenchar():
+    return str(int((time.time() + 0.5) * 1000))[-10:]
+
+
+def iso_datetime(date_obj=None):
+    if date_obj is None:
+        date_obj = datetime.now()
+    return date_obj.strftime("%Y-%m-%dT%H:%M:%S")
+
+
+def timestamp_string():
+    return datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M')
